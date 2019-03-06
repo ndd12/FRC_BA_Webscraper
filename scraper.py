0293c963b790b9c6042f53c724992018aa00c69b
ndd12 <douglas.noah7344@gmail.com>
2019-03-06 10:50:48 -0600

    Clean up of code formatting and added more useful comments.

--- scraper.py
+++ scraper.py
@@ -4,18 +4,15 @@ from xlwt import Workbook
 
 tba = tbapy.TBA('3WEGx9vYItqOFTwzOjip8LwmwQ4VpCJCfS0jlPdlqOP76XFkcEh3x66i2HzcRrq5')
 
+
 # Find the average score for an event
-def eventAverage(event):
+def event_average(event):
     dictionary = tba.event_insights(event)
     return round(dictionary['qual']['average_score'])
 
-# Find the average winning score for an event
-def winScore(event):
-    dictionary = tba.event_insights(event)
-    return round(dictionary['qual']['average_win_score'])
 
 # calculate the average points a team scored at a given event
-def teamAverage(team, event):
+def team_average(team, event):
     # import information about the matches a team had at an event
     dictionary = tba.team_matches(team, event)
     # variables for calculating averages
@@ -34,6 +31,7 @@ def teamAverage(team, event):
     return round(mySum / count)
 
 
+# note: rocket() is called within team_report(), and not directly called within event_report()
 def rocket(team, event, sheet,column):
     # 3 lists to hold the results of every match related to that specific part of the rocket
     lowerRocket = list()
@@ -41,11 +39,15 @@ def rocket(team, event, sheet,column):
     upperRocket = list()
     # dictionary used to hold information about teams matches
     dictionary = tba.team_matches(team, event)
-
+    # iterate through every match for given team
     for i in dictionary:
+        # separate blue and red information for current match
         breakDownBlue = i['score_breakdown']['blue']
         breakDownRed = i['score_breakdown']['red']
+
+        # if team is on red for current match, read information on red alliance, otherwise, read in blue information
         if ('frc' + str(team)) in (i['alliances']['red']['team_keys']):
+            # Read in information for all low rocket zones in the match, then do the same for middle slot and high slot
             lowerRocket.append(breakDownRed['lowLeftRocketNear'])
             lowerRocket.append(breakDownRed['lowLeftRocketFar'])
             lowerRocket.append(breakDownRed['lowRightRocketNear'])
@@ -75,12 +77,13 @@ def rocket(team, event, sheet,column):
             upperRocket.append(breakDownRed['topLeftRocketFar'])
             upperRocket.append(breakDownRed['topRightRocketNear'])
             upperRocket.append(breakDownRed['topRightRocketFar'])
-
-    lowerRocketScore=((round((lowerRocket.count('Panel') / len(lowerRocket)) * 100))+(round((lowerRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))/2
+    # algorithm for estimating a teams ability at each rocket level, and then an overall rocket rating
+    lowerRocketScore=((round((lowerRocket.count('Panel') / len(lowerRocket)) * 100)) + (round((lowerRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))/2
     middleRocketScore=((round((middleRocket.count('Panel') / len(lowerRocket)) * 100))+(round((middleRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))/2
     upperRocketScore=((round((upperRocket.count('Panel') / len(lowerRocket)) * 100))+(round((upperRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))/2
     rocketOVR=lowerRocketScore+(2*middleRocketScore)+(3 * upperRocketScore)
 
+    # once information has been calculated, write into the sheet for event_report
     sheet.write(4, column, (round((lowerRocket.count('Panel') / len(lowerRocket)) * 100)))
     sheet.write(5, column, (round((lowerRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))
     sheet.write(6, column, (round((middleRocket.count('Panel') / len(lowerRocket)) * 100)))
@@ -89,15 +92,30 @@ def rocket(team, event, sheet,column):
     sheet.write(9, column, (round((upperRocket.count('PanelAndCargo') / len(lowerRocket)) * 100)))
     sheet.write(10,column, (round(rocketOVR)))
 
-def teamReport(team, event,sheet,column):
-    average = teamAverage(team, event)
+
+# return a sorted list of the team numbers competing at a given event
+def event_teams(event):
+    list = tba.event_teams(event)
+    myList=[]
+    for i in list:
+        teamKey = i.key
+        team = str(teamKey).replace("frc", "")
+        number = int(team)
+        myList.append(number)
+    myList.sort()
+    return myList
+
+
+# write the information for given team into the event report
+def team_report(team, event, sheet, column):
+    average = team_average(team, event)
     sheet.write(0, column, (str(team)))
-    sheet.write(1, column, (average))
-    sheet.write(2, column, average-(eventAverage(event)))
+    sheet.write(1, column, average)
+    sheet.write(2, column, average-(event_average(event)))
     rocket(team, event, sheet, column)
 
 
-def eventReport(event):
+def event_report(event):
     # create new spreadsheet in project folder
     wb = Workbook()
     sheet1 = wb.add_sheet('sheet1')
@@ -111,24 +129,12 @@ def eventReport(event):
     sheet1.write(7, 0, "Middle Rocket Panel and Cargo Percentage: ")
     sheet1.write(8, 0, "Upper Rocket Panel Percentage: ")
     sheet1.write(9, 0, "Upper Rocket Panel and Cargo Percentage: " )
-    sheet1.write(10,0, "OVR Rocket Rating:")
+    sheet1.write(10, 0, "OVR Rocket Rating:")
+
     # generate a new team report for every team at the given event
-    for i in range(1,len(event_teams(event))+1):
-        teamReport(event_teams(event)[i-1],event,sheet1,i)
+    for i in range(1, len(event_teams(event))+1):
+        team_report(event_teams(event)[i-1], event, sheet1, i)
 
     # save the event report after all team reports have been calculated
     wb.save(str(event)+".xls")
-
-# return a sorted list of the team numbers competing at a given event
-def event_teams(event):
-    list = tba.event_teams(event)
-    myList=[]
-
-    for i in list:
-        teamKey=i.key
-        team=str(teamKey).replace("frc","")
-        number=int(team)
-        myList.append(number)
-    myList.sort()
-    return myList
 
